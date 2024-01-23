3b8e942d4a8629992ae566a05ed2f5f8c8c4fc0f
hanifFaiq <haniffaiq95@gmail.com>
2024-01-24 00:13:11 +0700

    Update : Json Data & prestasi card

--- src/components/About/Prestasi.jsx
+++ src/components/About/Prestasi.jsx
@@ -4,34 +4,78 @@ import AppContext from "../../context/AppContext";
 import Foto from "../../assets/img/Rectangle108school.png";
 import Wisuda from "../../assets/icon/ph_student-light.svg";
 import Logo from "../../assets/icon/ant-design_user-outlined.svg";
+import achivementLogo from "../../assets/img/achivement.jpg";
 import Loc from "../../assets/icon/loc.svg";
 import Telf from "../../assets/icon/telfon.svg";
 import data from "../../assets/json/data.json"
 
+// Fungsi untuk mengonversi string tanggal ke objek Date
+const parseDate = (dateString) => {
+  const [day, month, year] = dateString.split('/').map(Number);
+  return new Date(year, month - 1, day);
+};
+
+// Fungsi untuk mengonversi objek Date ke string tanggal dengan format DD/MM/YYYY
+const formatDate = (dateObject) => {
+  const day = dateObject.getDate();
+  const month = dateObject.getMonth() + 1;
+  const year = dateObject.getFullYear();
+  return `${day}/${month}/${year}`;
+};
+
 const itemsPerPage = 6;
-const PrestasiComponent = ({ author, date, title, description, location }) => (
+const PrestasiComponent = ({ author, created_at, title, description, category, location, date }) => (
   <button className="border rounded-lg p-3 flex flex-col">
     <img className="flex justify-center w-full" src={Foto} alt="foto" />
     <div className="text-start text-2xl mt-5">{title}</div>
     <div className="flex gap-3 text-start mt-3">
       <img src={Logo} alt="logo" />
-      <div className="text-[#8d8f91] flex-grow">{author}</div>
+      <div className="text-[#8d8f91] flex-grow">
+        {Array.isArray(author) ? (
+          author.map((authorItem, index) => (
+            <div key={index}>{authorItem}</div>
+          ))
+        ) : (
+          author
+        )}
+      </div>
     </div>
+
+    <div className="flex gap-3 text-start mt-3">
+      {/* <img src={Logo} alt="logo" /> */}
+      <div className="text-[#8d8f91] flex-grow">
+        {Array.isArray(category) ? (
+          category.map((categoryItem, index) => (
+            <div key={index}>{categoryItem}</div>
+          ))
+        ) : (
+          category
+        )}
+      </div>
+    </div>
+
+
     <div className="flex gap-3 text-start mt-3">
       <div className="text-[#8d8f91] flex-grow">{description}</div>
     </div>
+    <div className="flex-grow" /> {/* Penambahan bagian ini */}
+
     <div className="flex gap-3 items-start text-start mt-3">
       <div className="text-[#8d8f91]">{location}</div>
     </div>
     <div className="flex gap-3 text-start mt-3">
-      <div className="text-[#8d8f91]">{date}</div>
+      <div className="text-[#8d8f91]">{created_at}</div>
     </div>
   </button>
 );
 
 const Prestasi = () => {
   const { globalState, updateGlobalState } = useContext(AppContext);
-  const sortedPrestasi = data.achievementsData.sort((a, b) => new Date(b.date) - new Date(a.date));
+  const sortedPrestasi = [...data.achievementsData].sort((a, b) => {
+    const dateA = parseDate(a.date);
+    const dateB = parseDate(b.date);
+    return dateB - dateA;
+  });
 
   const [currentPage, setCurrentPage] = useState(1);
 
@@ -50,7 +94,7 @@ const Prestasi = () => {
       <div className="p-8">
         <div className="text-center text-5xl font-bold mb-10">Prestasi Unggulan</div>
         <div className="grid grid-cols-3 gap-10">
-          {currentPrestasi .map((achievement, index) => (
+          {currentPrestasi.map((achievement, index) => (
             <PrestasiComponent key={index} {...achievement} />
           ))}
         </div>
