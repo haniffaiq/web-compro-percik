110dd5f8529f9420f1fb62bf992d34eaeef09d79
syahdan41 <syahdantriantoro41@gmail.com>
2024-01-26 02:06:52 +0700

    syahdan update responsive page management

--- src/components/Management/ManagementTittle.jsx
+++ src/components/Management/ManagementTittle.jsx
@@ -15,7 +15,7 @@ const ManagementTittle = () => {
         <div className="deskripsi-management-container">
           <p>
             Norem ipsum dolor sit amet, consectetur adipiscing elit. Etiam eu turpis molestie, dictum est a, mattis tellus. Sed dignissim, metus nec fringilla accumsan, risus sem sollicitudin lacus, ut interdum tellus elit sed risus.
-            Maecenas eget condimentum velit, sit amet feugiat lectus. Class aptent taciti sociosqu ad litora torquent{" "}
+            Maecenas eget condimentum velit, sit amet feugiat lectus. Class aptent taciti sociosqu ad litora torquent.
           </p>
         </div>
       </div>
--- src/components/Management/TopSectionPicture.jsx
+++ src/components/Management/TopSectionPicture.jsx
@@ -12,15 +12,13 @@ const TopSectionPict = () => {
     {
       namaLengkapManajemen: "Ir. Budiono Kartohadiprodjo",
       jabatanManajemen: "Ketua",
-      deskripsiManajemen:
-        "Norem ipsum dolor sit amet, consectetur adipiscing elit. Etiam eu turpis molestie, dictum est a, mattis tellus. Sed dignissim, metus nec fringilla accumsan, risus sem sollicitudin lacus, ut interdum tellus elit sed risus. Maecenas eget condimentum velit, sit amet feugiat lectus. Class aptent taciti sociosqu ad litora torquent",
+      deskripsiManajemen: "Norem ipsum dolor sit amet, consectetur adipiscing elit. Etiam eu turpis molestie, dictum est a, mattis tellus.",
       imageSourceManajemen: ManajemenImg,
     },
     {
       namaLengkapManajemen: "Dr. Setiawan Nazir, M.Kes",
       jabatanManajemen: "Sekretaris",
-      deskripsiManajemen:
-        "Norem ipsum dolor sit amet, consectetur adipiscing elit. Etiam eu turpis molestie, dictum est a, mattis tellus. Sed dignissim, metus nec fringilla accumsan, risus sem sollicitudin lacus, ut interdum tellus elit sed risus. Maecenas eget condimentum velit, sit amet feugiat lectus. Class aptent taciti sociosqu ad litora torquent",
+      deskripsiManajemen: "Norem ipsum dolor sit amet, consectetur adipiscing elit. Etiam eu turpis molestie, dictum est a, mattis tellus.",
       imageSourceManajemen: ManajemenImg,
     },
   ];
