b306cdb70752193fd35d41f40f7c45a632ff32a4
Syahdan Triantoro <syahdantriantoro41@gmail.com>
2024-03-26 20:20:46 +0700

    syahdan progres modal

--- src/components/Management/TopSectionPicture.jsx
+++ src/components/Management/TopSectionPicture.jsx
@@ -1,10 +1,22 @@
 import React, { useContext, useState } from "react";
 // import { Link } from "react-router-dom";
 import AppContext from "../../context/AppContext";
+import Modal from "../Modal/Modal";
 import "./ManagementStyle.css";
 
 const TopSectionPict = ({ selectedButton, manajemenData }) => {
   const { globalState, updateGlobalState } = useContext(AppContext);
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [selectedManagement, setSelectedManagement] = useState(null);
+
+  const openModal = (managementData) => {
+    setSelectedManagement(managementData);
+    setIsModalOpen(true);
+  };
+
+  const closeModal = () => {
+    setIsModalOpen(false);
+  };
   const filteredData = manajemenData.filter((item) => item.jabatanManajemen === selectedButton);
 
   if (filteredData.length === 0) {
@@ -13,8 +25,8 @@ const TopSectionPict = ({ selectedButton, manajemenData }) => {
 
   return (
     <div>
-      <div className="top-section-pict-container">
-        {filteredData.map(({ id, namaLengkapManajemen, jabatanManajemen, deskripsiManajemen, imageSourceManajemen }) => (
+      {filteredData.map(({ id, namaLengkapManajemen, jabatanManajemen, deskripsiManajemen, imageSourceManajemen }) => (
+        <div className="top-section-pict-container" onClick={() => openModal({ namaLengkapManajemen, deskripsiManajemen })}>
           <div className="pict-list-management-wrapper">
             <div className="management-image-wrapper" key={id}>
               <img src={require(`../../assets/${imageSourceManajemen}`)} alt="img" />
@@ -24,8 +36,17 @@ const TopSectionPict = ({ selectedButton, manajemenData }) => {
               <p className="overlay-text-management-desc">{deskripsiManajemen}</p>
             </div>
           </div>
-        ))}
-      </div>
+        </div>
+      ))}
+      <Modal isOpen={isModalOpen} onClose={closeModal}>
+        {selectedManagement && (
+          <>
+            <h2>{selectedManagement.namaLengkapManajemen}</h2>
+            <p>{selectedManagement.deskripsiManajemen}</p>
+            <button onClick={closeModal}>tutup</button>
+          </>
+        )}
+      </Modal>
     </div>
   );
 };
